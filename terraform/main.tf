d44cf55fc6f466a31faeb044791ab070e3d2d466
Michael Main <michaelanthonymain@gmail.com>
2026-01-19 13:08:28 -0600

    create ecs cluster and task definition

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,53 @@ resource "aws_ecr_repository" "simpsons" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecs_cluster" "simpsons" {
+  name = "simpsons-cluster"
+}
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "simpsons-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "simpsons" {
+  family                   = "simpsons-simulator"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "simpsons-simulator"
+      image     = "${aws_ecr_repository.simpsons.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 4567
+          hostPort      = 4567
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
